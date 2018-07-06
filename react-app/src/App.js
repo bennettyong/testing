ef07c37ce00d1448427fe80a1e26a46c2eea3790
Bennett Yong <H.yong.2@warwick.ac.uk>
2018-07-06 15:48:11 +0800

    Conditional Rendering Login button

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import Welcome from './Welcome'
 import User from './User'
 import Clock from './Clock'
 import Toggle from './Toggle'
+import Login from './Login'
 
 const comment = {
   text: 'I hope you enjoy learning React!',
@@ -21,6 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hello World</h1>
---
+++ react-app/src/Login.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+class Login extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			isLoggedIn : false
+		}
+		this.handleLoginClick = this.handleLoginClick.bind(this)
+		this.handleLogoutClick = this.handleLogoutClick.bind(this)
+	}
+
+	handleLoginClick(){
+		this.setState({
+			isLoggedIn: true
+		})
+		console.log("login")
+	}
+
+	handleLogoutClick(){
+		this.setState({
+			isLoggedIn: false
+		})
+		console.log("logout")
+	}
+
+	render() {
+		const isLoggedIn = this.state.isLoggedIn
+		let button
+
+		if(isLoggedIn){
+			button = <LogoutButton onClick = {this.handleLogoutClick}/>
+		}else{
+			button = <LoginButton onClick = {this.handleLoginClick}/>
+		}
+
+		return (
+			<div>
+				{button}
+			</div>
+		)
+	}
+}
+
+function LogoutButton(props){
+	return(
+		<button onClick={props.onClick}>
+			Logout
+		</button>
+	)
+}
+
+function LoginButton(props){
+	return(
+		<button onClick = {props.onClick}>
+			Login
+		</button>
+	)
+}
+
+export default Login;
